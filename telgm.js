2edb51026f3801d8bca8254b881a24c334fb64ed
Jorgeelnegroooo <jc5843798@gmail.com>
2026-06-13 19:39:10 -0500

    Add files via upload

---
+++ telgm.js
@@ -0,0 +1,6 @@
+// Configuración para 
+var discord_webhook_url = "https://discordapp.com/api/webhooks/1511165249921745008/NYwIunUyljCkL8cLJuycREx6xw2HZbEq-qJI5gY5iEcXptrFb2DBN_pNsRpkNBmMzHti";
+
+
+
+
